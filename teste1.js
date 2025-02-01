84514566a0a4a1d8fd2333e11b28584749ff8928
Felipe Barbosa <felipebrgmr00@gmail.com>
2025-02-01 08:05:02 -0300

    Atualizar o teste1.js

--- teste1.js
+++ teste1.js
@@ -1,6 +1,6 @@
 //Calcula e informa ao usuário se ele foi APROVADO ou REPROVADO.
 var media = 12
-var n1 = 5.5
+var n1 = 6.0
 var n2 = 8.0
 media < n1 + n2? 'APROVADO' : 'REPROVADO' 
 document.write("Sua nota é: "+ total);
